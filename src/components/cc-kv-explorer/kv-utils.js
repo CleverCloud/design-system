CleverCloud/clever-components

fix(cc-kv-explorer): fix race condition on key details loading using a shared `Semaphore`

src/components/cc-kv-explorer/kv-utils.js
@@ -50,3 +50,32 @@ function convertKvMatchToRegex(match) {
 
   return new RegExp(`^${reg}$`);
 }
+
+class Semaphore {
+  constructor() {
+    /** @type {AbortController} */
+    this.abortCtrl = null;
+  }
+
+  /**
+   * @param {(...args: Array<any>) => Promise<T>} fun
+   * @returns {Promise<T>}
+   * @template T
+   */
+  async run(fun) {
+    this.abortCtrl?.abort();
+    this.abortCtrl = new AbortController();
+
+    return new Promise((resolve, reject) => {
+      fun(this.abortCtrl.signal)
+        .then(resolve)
+        .catch((e) => {
+          if (!(e instanceof DOMException && e.name === 'AbortError')) {
+            reject(e);
+          }
+        });
+    });
+  }
+}
+
+export const kvSharedSemaphore = new Semaphore();

CleverCloud/design-system

src/components/cc-kv-explorer/kv-utils.js
@@ -50,3 +50,32 @@ function convertKvMatchToRegex(match) {
 
   return new RegExp(`^${reg}$`);
 }
+
+class Semaphore {
+  constructor() {
+    /** @type {AbortController} */
+    this.abortCtrl = null;
+  }
+
+  /**
+   * @param {(...args: Array<any>) => Promise<T>} func
+   * @returns {Promise<T>}
+   * @template T
+   */
+  async run(func) {
+    this.abortCtrl?.abort();
+    this.abortCtrl = new AbortController();
+
+    return new Promise((resolve, reject) => {
+      func(this.abortCtrl.signal)
+        .then(resolve)
+        .catch((e) => {
+          if (!(e instanceof DOMException && e.name === 'AbortError')) {
+            reject(e);
+          }
+        });
+    });
+  }
+}
+
+export const kvSharedSemaphore = new Semaphore();